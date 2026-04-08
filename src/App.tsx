46fd483f4307da77428e0de773a4d6a0ac6908ca
tauffanpc <tauffanabdi.pc@gmail.com>
2026-04-09 02:51:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from './lib/LanguageContext';
 import Intro from './pages/Intro';
 import Landing from './pages/Landing';
 import Archive from './pages/Archive';
@@ -8,15 +9,17 @@ import DiaryHistory from './pages/DiaryHistory';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/home" element={<Landing />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/diary-history" element={<DiaryHistory />} />
-      </Routes>
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/home" element={<Landing />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/diary-history" element={<DiaryHistory />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
   );
 }
